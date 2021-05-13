df2f02f21bb843d0d1255e0d19ad276bd6f98668
Ayyushhh <ayushj820@gmail.com>
2021-05-13 21:23:27 +0530

    fifth commit

--- linked_list.h
+++ linked_list.h
@@ -194,3 +194,18 @@ bool detectLoop(Node* head)
 	return false;
 }
 
+//length of the loop
+int countNodesinLoop(Node* head)
+{
+     Node *slow_p = head, *fast_p = head;
+
+    while (slow_p && fast_p && fast_p->next)
+    {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if (slow_p == fast_p)
+            return countNodes(slow_p);
+    }
+    return 0;
+}
+
